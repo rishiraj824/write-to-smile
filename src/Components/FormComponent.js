11a5f3f8df21cbaeeb73ab03b12c926febc801ff
Thiyagaraj T <tstream.h@outlook.com>
2017-09-01 20:33:07 +0530

    improve form css and add hint messages

--- src/Components/FormComponent.js
+++ src/Components/FormComponent.js
@@ -7,10 +7,10 @@ class FormComponent extends Component {
   render() {
     return (
         <Form>
-        <legend>Share your problems with us!</legend>
-        <Input hint="Email" />
-        <Textarea hint="Enter your message" />
-        <Button variant="raised">Submit</Button>
+        <legend>Share your problem with us!</legend>
+        <Textarea hint="Share your problem. We'll keep it a secret. *Promise*. Our team will reply back anonymously. It is completely safe & automated. " />
+        <Input hint="Email ID (You will receive our response on this) " />
+        <Button   className= "outlineButton" >Submit</Button>
       </Form>
     );
   }
