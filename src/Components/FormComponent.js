c77f8a67e8cc93df5438bfe4ec19c5ef49eb6846
Thiyagaraj T <tstream.h@outlook.com>
2017-09-05 13:39:39 +0530

    fix form to ack state function

--- src/Components/FormComponent.js
+++ src/Components/FormComponent.js
@@ -6,6 +6,10 @@ import Button from 'muicss/lib/react/button';
 import Animate from 'react-simple-animate';
 class FormComponent extends Component {
   render() {
+
+    var buttonStyle = {
+    
+    };
     return (
       <Animate durationSeconds={0.2}
          startAnimation
@@ -21,7 +25,7 @@ class FormComponent extends Component {
         <legend className = "white" >Share your problem with us!</legend>
         <Textarea hint="Share your problem. We'll keep it a secret. *Promise*. Our team will reply back anonymously. It is completely safe & automated. " />
         <Input hint="Email ID (You will receive our response on this) " />
-        <Button   className= "outlineButton" onClick = {this.props.showAck} >Submit</Button>
+        <div  className= "outlineButton" onClick= {this.props.onClick} >Submit</div>
       </Form>
       </Animate>
     );
